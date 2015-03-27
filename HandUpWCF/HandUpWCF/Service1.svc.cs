071814d7e614a1bb75131583a22c30bc54e26515
CarelCS <carel.c.scholtz@gmail.com>
2015-03-27 10:06:41 +0200

    Update

--- HandUpWCF/HandUpWCF/Service1.svc.cs
+++ HandUpWCF/HandUpWCF/Service1.svc.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using MySql.Data.MySqlClient;
 
 namespace HandUpWCF {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
