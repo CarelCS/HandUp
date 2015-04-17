cc2dedee9c2d0dffb75fa17478ae41eb34120d2e
heindurow <hein.durow@gmail.com>
2015-04-17 13:35:13 +0200

    WCF

--- HandUpWCF/HandUpWCF/Menu.cs
+++ HandUpWCF/HandUpWCF/Menu.cs
@@ -11,15 +11,43 @@ namespace HandUpWCF {
             tblMenu aMenu = new tblMenu();
             aMenu.addEquals(tblMenu._FKIPROVIDERID, sProviderID);
             DataSet aDataSet = aMenu.executeSelectDataSet();
+            aDataSet.Tables[0].TableName = "Menu";
+
+            tblluMenugroups aMenuGroup = new tblluMenugroups();
+            DataSet dsGroupNames = aMenuGroup.executeCustomSQLDataSet("SELECT distinct " + tblluMenugroups._PKIMENUGROUPID + "," + tblluMenugroups._SMENUGROUPNAME + "," + tblluMenugroups._SMENUGROUPDESCRIPTION +
+                " FROM tbllumenugroups,tblmenu" +
+                " WHERE tbllumenugroups.PKiMenuGroupID=tblmenu.FKiMenuGroupID and handup.tblmenu.FKiProviderID=" + sProviderID);
+            dsGroupNames.Tables[0].TableName = "MenuGroup";
+            aDataSet.Tables.Add(dsGroupNames.Tables[0].Copy());
 
             foreach (DataRow aMenuDataRow in aDataSet.Tables[0].Rows) {
                 tblluSubmenus aSubMenu = new tblluSubmenus();
                 aSubMenu.addEquals(tblluSubmenus._FKIMENUID, Convert.ToInt32(aMenuDataRow[tblMenu._PKIMENUID]));
                 DataSet dsSubMenus = aSubMenu.executeSelectDataSet();
-                foreach (DataRow aSubMenuDataRow in dsSubMenus.Tables[0].Rows) {
 
+                Dictionary<string, DataSet> dicSubMenuGroup = new Dictionary<string, DataSet>();
+
+                foreach (DataRow aSubMenuDataRow in dsSubMenus.Tables[0].Rows) {
+                    tblluSubmenugrouptype aSubMenuGroup = new tblluSubmenugrouptype((int)aSubMenuDataRow[tblluSubmenus._FKISUBMENUGROUPTYPEID]);
+                    if (dicSubMenuGroup.ContainsKey(aSubMenuGroup.sSubMenuGroupTypeName)) {
+                        DataSet aSubMenuPerGroupDS = dicSubMenuGroup[aSubMenuGroup.sSubMenuGroupTypeName];
+                        aSubMenuPerGroupDS.Tables[aSubMenuGroup.sSubMenuGroupTypeName].ImportRow(aSubMenuDataRow);
+                    }
+                    else {
+                        DataSet aSubMenuPerGroupDS = new DataSet();
+                        aSubMenuPerGroupDS.Tables.Add(aSubMenuGroup.sSubMenuGroupTypeName);
+                        foreach (DataColumn aDataColumn in dsSubMenus.Tables[0].Columns) {
+                            aSubMenuPerGroupDS.Tables[aSubMenuGroup.sSubMenuGroupTypeName].Columns.Add(aDataColumn.ToString());
+                        }
+                        aSubMenuPerGroupDS.Tables[aSubMenuGroup.sSubMenuGroupTypeName].ImportRow(aSubMenuDataRow);
+                        dicSubMenuGroup.Add(aSubMenuGroup.sSubMenuGroupTypeName, aSubMenuPerGroupDS);
+                    }
+                }
+                foreach (KeyValuePair<string, DataSet> aKeyPair in dicSubMenuGroup) {
+                    aDataSet.Tables.Add(aKeyPair.Value.Tables[0].Copy());
                 }
             }
+
 
 
             // DataAdapters newAdapter = new DataAdapters();
